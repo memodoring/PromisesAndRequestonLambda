088cfdec385c94eae9575bb08f7593f8325ee523
Memo Doring <memo@doring.me>
2016-03-16 18:08:43 -0700

    Add parseJSON, move functions out of handler, remove commented lines

--- main.js
+++ main.js
@@ -3,57 +3,56 @@ var fs = require('fs');
 var Promise = require('promise');
 var request = require('request');
 
+  requestPage('http://jsonplaceholder.typicode.com/users').then(logRes);
 
 exports.handler = function(event, context) {
 
-  //console.log('TEST');
-  //readFile('sample.txt','utf8').then(console.log);
-  //readFile('sample.txt','utf8').then(logTest).then(console.log);
-  readFile('sample.txt','utf8').then(logTest);
-  //requestPage('http://www.freecodecamp.com/the-fastest-web-page-on-the-internet').then(logTest).then(console.log).catch(console.log);
-  //requestPage('http://www.freecodecamp.com/the-fastest-web-page-on-the-internet').then(logTest).then(context.succeed(event.key3)).catch(console.log);
-
-  requestPage('http://www.freecodecamp.com/the-fastest-web-page-on-the-internet').then(logTest).catch(context.fail);
-  requestPage('http://jsonplaceholder.typicode.com/users').then(logTest).then(context.succeed).catch(context.fail);
-
-  //console.log('value1 =', event.key1);
-  //console.log('value2 =', event.key2);
-  //console.log('value3 =', event.key3);
-  //context.succeed(event.key1);  // Echo back the first key value
-  // context.fail('Something went wrong');
-
-  function readFile(filename, enc){
-    //console.log("readFile");
-    return new Promise(function(fulfill, reject){
-      fs.readFile(filename, enc, function(err, res){
-        if(err) reject(err);
-        else fulfill(res);
-      });
-    });
-  }
-
-  function requestPage(address){
-    //console.log("requestPage");
-    return new Promise(function(fulfill, reject){
-      request(address, function(err, res, body){
-        if(err) reject(err);
-        else{
-          fulfill(res.statusCode);
-        }
-      });
-    });
-  }
-
-  function logTest(thing){
-    //console.log("logTest");
-    return new Promise(function(fulfill, reject){
-      try {
-        console.log(thing);
-      } catch (err) {
-        reject(err);
-      }
-      fulfill(thing);
-    });
-  }
-
+  //readFile('package.json','utf8').then(parseJSON).then(logRes).catch(logRes);
+  requestPage('http://www.freecodecamp.com/the-fastest-web-page-on-the-internet').then(logRes).catch(context.fail);
+  requestPage('http://jsonplaceholder.typicode.com/users').then(logRes).then(context.succeed).catch(context.fail);
 };
+
+function requestPage(address){
+  return new Promise(function(fulfill, reject){
+    request(address, function(err, res, body){
+      if(err) reject(err);
+      else{
+        fulfill(res.statusCode);
+      }
+    });
+  });
+}
+
+function readFile(filename, enc){
+  return new Promise(function(fulfill, reject){
+    fs.readFile(filename, enc, function(err, res){
+      if(err) reject(err);
+      else fulfill(res);
+    });
+  });
+}
+
+function readJSON(file){
+  return readFile(file,'utf8').then(JSON.parse);
+}
+
+function logRes(thing){
+  return new Promise(function(fulfill, reject){
+    try {
+      console.log(thing);
+    } catch (e) {
+      reject(e);
+    }
+    fulfill(thing);
+  });
+}
+
+function parseJSON(toParse){
+  return new Promise(function(fulfill, reject) {
+    try {
+      fulfill(JSON.parse(toParse));
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
